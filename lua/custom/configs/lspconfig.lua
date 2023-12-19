076618eb97f8be104c628b0d3534779625660df2
檀轶步棋 <57583509+oissevalt@users.noreply.github.com>
2023-12-19 15:31:59 +0800

    Upload my configuration

---
+++ lua/custom/configs/lspconfig.lua
@@ -0,0 +1,39 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+local util = require "lspconfig/util"
+
+-- if you just want default config for the servers then put them in a table
+local servers = { "html", "cssls", "tsserver", "clangd", "gopls" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+lspconfig.rust_analyzer.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"rust"},
+  root_dir = util.root_pattern("Cargo.toml"),
+  settings = {
+    ['rust_analyzer'] = {
+      cargo = {
+        allFeatures = true,
+      },
+      inlayHints = {
+        enable = true,
+        showParameterNames = true,
+      },
+      diagnostics = {
+        enable = true,
+      }
+    }
+  }
+})
+
+-- 
+-- lspconfig.pyright.setup { blabla}
